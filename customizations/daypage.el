7aac52d2540300c030a74ae6f3e796de9b9dd443
Joseph Wright <joegle@gmail.com>
2017-01-17 17:34:43 -0500

    first pass convert

---
+++ customizations/daypage.el
@@ -0,0 +1,14 @@
+(setq daypage-path "~/Documents/notes/")
+
+(define-key daypage-mode-map (kbd "<C-left>") 'daypage-prev)
+(define-key daypage-mode-map (kbd "<C-right>") 'daypage-next)
+
+;; (define-key daypage-mode-map (kbd "<C-up>") 'daypage-prev-week)
+;; (define-key daypage-mode-map (kbd "<C-down>") 'daypage-next-week)
+;; (define-key daypage-mode-map "\C-c." 'daypage-time-stamp)
+;;
+;; (global-set-key [f11] 'todays-daypage)
+;; (global-set-key [f10] 'yesterdays-daypage)
+
+(global-set-key "\C-con" 'todays-daypage)
+(global-set-key "\C-coN" 'find-daypage)
---
+++ customizations/ui.el
@@ -0,0 +1,12 @@
+;disable backups
+(setq backup-inhibited t)
+
+;disable auto save
+(setq auto-save-default nil)
+
+;; Show line numbers
+(global-linum-mode)
+
+(global-visual-line-mode t)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
