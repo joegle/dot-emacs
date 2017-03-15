85f84751053329039c1da2babd8b7e234a0b6864
Joseph Wright <wrightj13@upmc.edu>
2017-03-15 13:27:15 -0400

    add move-text

--- customizations/editing.el
+++ customizations/editing.el
@@ -1,8 +1,13 @@
 (require 'use-package)
 
+(require 'move-text)
+(move-text-default-bindings)
+
 (bind-keys*
  ("M-DEL" . backward-delete-char)
- ("DEL" . backward-kill-word))
+ ("DEL" . backward-kill-word)
+ ("M-j" . move-text-down)
+ ("M-k" . move-text-up))
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 
