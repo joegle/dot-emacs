0ed845a78347d1b73d6c91301b42cc0a52ed8c19
Joseph Wright <wrightj13@upmc.edu>
2017-03-22 09:06:57 -0400

    set javascript indent width

--- customizations/setup-javascript.el
+++ customizations/setup-javascript.el
@@ -1,3 +1,9 @@
 
 (setq js2-basic-offset 2)
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
 
+
+(add-hook 'json-mode-hook
+	  (lambda ()
+	    (make-local-variable 'js-indent-level)
+	    (setq js-indent-level 2)))
