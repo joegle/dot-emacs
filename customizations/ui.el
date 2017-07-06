ae3d18986bfecfec0ced17b84c58fe983db0046d
Joseph Wright <joegle@gmail.com>
2017-07-06 14:38:18 -0400

    add 80 char rule

--- customizations/ui.el
+++ customizations/ui.el
@@ -19,3 +19,14 @@
 ; disable the toolbar with icons
 (tool-bar-mode -1)
 
+
+(require 'fill-column-indicator)
+
+(setq-default fill-column 80)
+(define-globalized-minor-mode my-global-fci-mode fci-mode turn-on-fci-mode)
+
+(my-global-fci-mode 1)
+
+(global-hl-line-mode 1)
+
+
