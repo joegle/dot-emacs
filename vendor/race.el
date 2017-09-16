7f103272679330344cdfff4cc4f43cbed7cffb12
Joseph Wright <joe@joegle.com>
2017-09-16 19:52:20 -0400

    add overlay function

--- vendor/race.el
+++ vendor/race.el
@@ -1,7 +1,7 @@
 ;; race-mode
 ;; Copyright 2017 Joseph Wright joe@joegle.com
 
-; Followed http://nullprogram.com/blog/2013/02/06/
+; ollowed http://nullprogram.com/blog/2013/02/06/
 
 (make-variable-buffer-local
  (defvar race-target 0
@@ -11,6 +11,13 @@
   "start the race"
   (interactive)
   (message "Race started!"))
+
+(defun race-target-overlay (@begin @end)
+    "highlight the target with overlay"
+    (interactive "r")
+    (progn
+      (overlay-put (make-overlay @begin @end) 'face '(:underline t :background "#5fd700" :box t :foreground "black"))
+      (setq mark-active nil )))
 
 (defun race-end ()
   "end the race"
@@ -27,6 +34,7 @@
   "Cursor movement racing game"
   :lighter " race"
   :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-c t") 'race-target-overlay)
             (define-key map (kbd "C-c s") 'race-start)
             (define-key map (kbd "C-c e") 'race-end)
             map))
