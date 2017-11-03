a277907d41117801db525e445c2b3e9dd4acb8ea
Joseph Wright <joe@joegle.com>
2017-11-02 20:04:28 -0400

    add custom mac code to interpret hexcode for M-S commands

---
+++ customizations/setup-mac.el
@@ -0,0 +1,14 @@
+(message "loading mac settings")
+
+; this is needed so I can reorder sections in org-mode
+; http://webframp.com/emacs/2013/02/22/fixing-emacs-bindings-on-the-in-iterm2/
+(define-key input-decode-map "\e[1;10A" [M-S-up])
+(define-key input-decode-map "\e[1;10B" [M-S-down])
+(define-key input-decode-map "\e[1;10C" [M-S-right])
+(define-key input-decode-map "\e[1;10D" [M-S-left])
+
+(define-key input-decode-map "\e[1;3A" [M-up])
+(define-key input-decode-map "\e[1;3B" [M-down])
+(define-key input-decode-map "\e[1;3C" [M-right])
+(define-key input-decode-map "\e[1;3D" [M-left])
+
