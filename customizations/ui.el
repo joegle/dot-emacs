f4627947bd03ce86bcd5f5047f82b5cfe24a64cc
Joseph Wright <joseph.wright@joegle.com>
2019-01-11 17:37:12 -0500

    Blink modeline on bell ring

--- customizations/ui.el
+++ customizations/ui.el
@@ -30,3 +30,14 @@
 (require 'zen-mode)
 
 (global-hl-line-mode 1)
+
+(set-face-background 'vertical-border "gray")
+(set-face-foreground 'vertical-border (face-background 'vertical-border))
+
+(setq ring-bell-function
+      (lambda ()
+        (let ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg))))
